bea6dd623d8b19c2751bd52433f73c47a36f2faf
shinkawadai <dai.shinkawa@a-olive.jp>
2023-06-01 18:12:13 +0900

    ローカルでAnalyticsをオフにする

--- plugins/analytics.ts
+++ plugins/analytics.ts
@@ -1,19 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAZqdPea0kV3-_H-g2vpC2YMtRStOiCC6M",
-  authDomain: "splatoon3-shuffle-weapons.firebaseapp.com",
-  projectId: "splatoon3-shuffle-weapons",
-  storageBucket: "splatoon3-shuffle-weapons.appspot.com",
-  messagingSenderId: "245051989191",
-  appId: "1:245051989191:web:edb70aedbaee52e3d1b2e2",
-  measurementId: "G-J3TS6H6J7Z"
-};
+if (process.env.NODE_ENV === 'production') {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAZqdPea0kV3-_H-g2vpC2YMtRStOiCC6M",
+    authDomain: "splatoon3-shuffle-weapons.firebaseapp.com",
+    projectId: "splatoon3-shuffle-weapons",
+    storageBucket: "splatoon3-shuffle-weapons.appspot.com",
+    messagingSenderId: "245051989191",
+    appId: "1:245051989191:web:edb70aedbaee52e3d1b2e2",
+    measurementId: "G-J3TS6H6J7Z"
+  };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  const app = initializeApp(firebaseConfig);
 
 
 // Initialize Analytics and get a reference to the service
-const analytics = getAnalytics(app);
+  const analytics = getAnalytics(app);
+} else {
+  console.log('Analytics is disabled')
+}
+
